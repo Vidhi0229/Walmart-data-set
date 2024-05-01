835103d27ec83263cb9321a4f04c032f67a30073
Vidhi <srivastavavidhi29@gmail.com>
2024-05-01 13:32:15 +0530

    edit

--- Walmart-Sales.py
+++ Walmart-Sales.py
@@ -3,16 +3,14 @@ import pandas as pd
 file = "Walmart_Sales.xlsx"
 sales_data = pd.read_excel(file)
 
-csv_file = "Walmart_Sales.csv"
-sales_data.to_csv(csv_file, index=False)
-
-
-sales_csv = pd.read_csv(csv_file)
-
 # A. Analyze the performance of sales and revenue at the city and branch level
 sales_data['Total'] = sales_data['Unit price'] * sales_data['Quantity']
-performance = sales_data.groupby(['City', 'Branch']).agg({'Total': 'sum'})
-print("\n performance of sales and revenue at the city and branch level\n", performance)
+
+total_quantity = sales_data['Quantity'].sum()
+t_unit = sales_data['Unit price'].sum()
+
+performance = sales_data.groupby(['City', 'Branch']).agg({'Quantity': 'sum', 'Unit price': 'sum' ,'Total': 'sum'})
+print("\nPerformance of sales and revenue at the city and branch level:\n", performance)
 
 # B. What is the average price of an item sold at each branch of the city 
 average_price = sales_data.groupby(['City', 'Branch'])['Unit price'].mean()
